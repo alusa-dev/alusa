948bb3e1e5fe788edef179e8d0bdd9d2ed3d417f
Alusa <gestao.alusa@gmail.com>
2026-08-13 16:17:46 -0400

    test: use esm import in mobile setup

--- apps/mobile/jest.setup.ts
+++ apps/mobile/jest.setup.ts
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 jest.mock('expo-secure-store', () => ({
   AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY: 'AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY',
   getItemAsync: jest.fn(),
@@ -6,7 +8,6 @@ jest.mock('expo-secure-store', () => ({
 }));
 
 jest.mock('expo-router', () => {
-  const React = require('react');
   return {
     Redirect: ({ href }: { href: string }) => React.createElement('Redirect', { href }),
     Stack: Object.assign(({ children }: { children: React.ReactNode }) => React.createElement(React.Fragment, null, children), {
